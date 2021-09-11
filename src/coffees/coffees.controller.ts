c1458ec7ab54e2d03ec26b77de12fe87b43d4ff2
Anthony Sim <anthonyysim@gmail.com>
2021-09-11 15:15:56 -0700

    initial

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,28 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+  @Get()
+  findAll() {
+    return 'This action returns all coffees';
+  }
+
+  @Get(':id')
+  findeOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.GONE)
+  create(@Body() body) {
+    return body;
+  }
+}
